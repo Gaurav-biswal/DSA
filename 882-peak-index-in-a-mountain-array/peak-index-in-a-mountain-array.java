62619cb27e23c363848dd1db6daef6d69e96878d
Gaurav-biswal <gauravbiswal1618@gmail.com>
2026-08-06 12:31:17 +0530

    Time: 0 ms (100.00%) | Memory: 80.1 MB (78.74%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -3,7 +3,7 @@ class Solution {
         int start = 0;
         int end = arr.length - 1;
 
-        while (start <= end){
+        while (start < end){
             int mid = start + (end - start) / 2;
 
             if (arr[mid] < arr[mid + 1]){
@@ -11,7 +11,7 @@ class Solution {
                 start = mid + 1;
             } else {
                 // descending part
-                end = mid - 1;
+                end = mid;
             }
         }
         return start; //or mid
